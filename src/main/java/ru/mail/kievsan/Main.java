2a20c2bf9e992e03cb35bc8a3def28ca31dcee95
Sergey Kievskiy <knhel7@mail.ru>
2023-10-01 05:23:32 +0300

    Init

--- src/main/java/ru/mail/kievsan/Main.java
+++ src/main/java/ru/mail/kievsan/Main.java
@@ -1,37 +1,31 @@
 package ru.mail.kievsan;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     final static String LETTERS = "RLRFR";
     final static  int ROUTE_LENGTH = 100 , AMOUNT_OF_THREADS = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
-        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
-            new Thread(() -> {
-                String route = generateRoute(LETTERS, ROUTE_LENGTH);
-                int frequency = (int) route.chars().filter(ch -> ch == 'R').count();
+    final static List<Thread> baseThreadList = new ArrayList<>();
 
-                synchronized (sizeToFreq) {
-                    sizeToFreq.put(frequency,
-                            sizeToFreq.containsKey(frequency) ?
-                            sizeToFreq.get(frequency) + 1 : 1);
-                    System.out.println(sizeToFreq.get(frequency) + 1);
-                }
-            }).start();
+    public static void main(String[] args) throws InterruptedException {
+        Thread printMaxFreq = getPrintMaxFreqThread();
+        printMaxFreq.start();
+
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            Thread baseThread = getSizeToFreqThread();
+            baseThreadList.add(baseThread);
+            baseThread.start();
         }
-        Map.Entry<Integer, Integer> max = sizeToFreq
-                .entrySet()
-                .stream()
-                .max(Map.Entry.comparingByValue())
-                .get();
-        System.out.println(
-                "Самое частое количество повторений " + max.getKey() +
-                " (встретилось " + max.getValue() + " раз)"
-        );
+
+        // ждём, когда все основные потоки завершатся
+        for (Thread thread : baseThreadList) {
+            thread.join();
+        }
+        // и прерываем печатающий поток
+        printMaxFreq.interrupt();
+
         System.out.println("Другие размеры: ");
         sizeToFreq
                 .entrySet()
@@ -40,6 +34,44 @@ public class Main {
                 .forEach(e -> System.out.println(
                         " - " + e.getKey() + " (" + e.getValue() + " раз)"
                         ));
+    }
+
+    private static Thread getSizeToFreqThread() {
+        return new Thread(() -> {
+            String route = generateRoute(LETTERS, ROUTE_LENGTH);
+            int frequency = (int) route.chars().filter(ch -> ch == 'R').count();
+
+            synchronized (sizeToFreq) {
+                sizeToFreq.put(frequency,
+                        sizeToFreq.containsKey(frequency) ?
+                                sizeToFreq.get(frequency) + 1 : 1);
+                sizeToFreq.notify();
+                // System.out.println(sizeToFreq);
+            }
+        });
+    }
+
+    private static Thread getPrintMaxFreqThread() {
+        return new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Map.Entry<Integer, Integer> max = sizeToFreq
+                            .entrySet()
+                            .stream()
+                            .max(Map.Entry.comparingByValue())
+                            .get();
+                    System.out.println(
+                            "Самое частое количество повторений " + max.getKey() +
+                            " (встретилось " + max.getValue() + " раз)"
+                    );
+                }
+            }
+        });
     }
 
     public static String generateRoute(String letters, int length) {
